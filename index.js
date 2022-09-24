a1123d9d01c8891450836b3a1133f014a21bd187
Benjamin Rittenhouse <benjamin.rittenhouse@temple.edu>
2022-09-24 13:43:47 -0400

    /blog returns all blog posts, /blog/post/:id adds blog by id

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const cors = require('cors')
 
 const db = require ('./db')
-const router = require('./router')
+const router = require('./routes/index')
+
+const getBlogs = require("./controller/getBlogs")
+const postBlog = require("./controller/PostBlog")
 
 const app = express()
 const port = 3001
@@ -14,5 +17,16 @@ app.use(express.json())
 db.on('error', console.error.bind(console, 'MongoDB connection failed...'))
 
 app.use('/api', router)
+
+// get the entire blog
+app.get("/blog", (req, res) => {
+    getBlogs(req, res);
+});
+
+// add blog post
+app.get("/blog/post/:id", (req, res) => {
+    req.body.id = req.params.id;
+    postBlog(req, res);
+});
 
 app.listen(port, () => console.log(`Server running on ${port}`))
